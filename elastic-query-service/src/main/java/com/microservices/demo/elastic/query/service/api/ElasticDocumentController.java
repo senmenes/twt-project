b066cd28796db46948672b7ba7eb49426336b728
senmenes <senmenes95@gmail.com>
2023-04-07 12:01:28 +0300

    added documentation and api versioning

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,12 @@ package com.microservices.demo.elastic.query.service.api;
 import com.microservices.demo.elastic.query.service.business.impl.TwitterElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +19,7 @@ import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/documents")
+@RequestMapping(path = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticDocumentController.class);
@@ -24,6 +30,15 @@ public class ElasticDocumentController {
         this.service = service;
     }
 
+    @Operation(summary = "To get all documents use this endpoint!")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Success!", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                             schema = @Schema(implementation = ElasticQueryServiceResponseModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found!"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error!")
+    })
     @GetMapping("/")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments(){
         List<ElasticQueryServiceResponseModel> response = service.getAllDocuments();
@@ -31,6 +46,15 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "To get document with id use this endpoint!")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Success!", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found!"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error!")
+    })
     @GetMapping("/{id}")
     public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id){
         ElasticQueryServiceResponseModel response = service.getDocumentById(id);
@@ -38,10 +62,47 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/get-document-by-text")
+    @Operation(summary = "To get all documents use this endpoint!")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Success!", content = {
+                    @Content(mediaType = "application/vnd.api.v2+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found!"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error!")
+    })
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id){
+        ElasticQueryServiceResponseModel response = service.getDocumentById(id);
+        LOGGER.info("ES returned doc with id {}", id);
+        return ResponseEntity.ok(transformModelToV2(response));
+    }
+
+    @Operation(summary = "To get documents with text use this endpoint!")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Success!", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found!"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error!")
+    })
+    @PostMapping("/get-document-by-text")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentsByText(@RequestBody @Valid ElasticQueryServiceRequestModel requestModel){
         List<ElasticQueryServiceResponseModel> response = service.getDocumentByText(requestModel.getText());
         LOGGER.info("ES returned {} of documents", response.size());
         return ResponseEntity.ok(response);
+    }
+
+    private static ElasticQueryServiceResponseModelV2 transformModelToV2(ElasticQueryServiceResponseModel response) {
+        ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2.builder()
+                .id(Long.parseLong(response.getId()))
+                .text(response.getText())
+                .userId(response.getUserId())
+                .createTime(response.getCreateTime())
+                .textInfo("Version 2")
+                .build();
+        responseModelV2.add(response.getLinks());
+        return responseModelV2;
     }
 }
--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.microservices.demo.elastic.query.service.config;
 
 import com.microservices.demo.config.ElasticUserConfigData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,8 +18,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final ElasticUserConfigData userConfigData;
 
+    @Value(value = "${security.paths-to-ignore}")
+    private String[] pathsToIgnore;
+
     public WebSecurityConfig(ElasticUserConfigData userConfigData) {
         this.userConfigData = userConfigData;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(pathsToIgnore);
     }
 
     @Override
